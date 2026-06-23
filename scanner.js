db9b5e60f599f1774b0a2a368001346780484994
pisuthmettvk-byte <pisuthmett.vk@gmail.com>
2026-06-23 13:12:57 +0700

    02

--- scanner.js
+++ scanner.js
@@ -67,7 +67,7 @@ async function startScanner() {
       {
         fps: 30,
         formatsToSupport: allFormats,
-        aspectRatio: 1.77,
+        aspectRatio: 16:9,
         experimentalFeatures: {
           useBarCodeDetectorIfSupported: true, // ใช้ตัวอ่านระดับฮาร์ดแวร์ของ Browser (อ่านติดไวขึ้นมาก)
         },
