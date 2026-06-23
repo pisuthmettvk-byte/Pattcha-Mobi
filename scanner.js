860ca8e1cf833d53e53b530f7c5ee5714c1adf68
pisuthmettvk-byte <pisuthmett.vk@gmail.com>
2026-06-24 00:13:01 +0700

    standard

--- scanner.js
+++ scanner.js
@@ -71,7 +71,7 @@ async function startScanner() {
     await html5QrCode.start(
       { facingMode: "environment" },
       {
-        fps: 30,
+        fps: 60,
         formatsToSupport: allFormats,
         aspectRatio: 1.77,
         experimentalFeatures: {
