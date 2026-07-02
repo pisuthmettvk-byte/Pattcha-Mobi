2ec840d616b6d27640df3cf2f9e4856baebe8b9b
pisuthmettvk-byte <pisuthmett.vk@gmail.com>
2026-07-03 02:41:34 +0700

    เค

--- data-connector.js
+++ data-connector.js
@@ -6,7 +6,7 @@ import {
 } from "https://www.gstatic.com/firebasejs/10.11.0/firebase-firestore.js";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBcDzLla0xp2lyAft2KbUPneBIj6_UgrSg",
+  apiKey: "",
   authDomain: "pattcha-project.firebaseapp.com",
   projectId: "pattcha-project",
   storageBucket: "pattcha-project.firebasestorage.app",
