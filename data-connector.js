4009dde992b55921a15ea834526e6b6ca57b28d5
pisuthmettvk-byte <pisuthmett.vk@gmail.com>
2026-07-30 01:23:47 +0700

    นิว

--- data-connector.js
+++ data-connector.js
@@ -18,7 +18,7 @@ import {
 } from "https://www.gstatic.com/firebasejs/10.11.0/firebase-firestore.js";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDJSD2e6CnRVlRaB9XwiqX7rbTqBQ8r8Oo",
+  apiKey: "AIzaSyCoktyV9rGaO39avYPqpo2gPqfMLnzM4SQ",
   authDomain: "pattcha-project.firebaseapp.com",
   projectId: "pattcha-project",
   storageBucket: "pattcha-project.firebasestorage.app",
